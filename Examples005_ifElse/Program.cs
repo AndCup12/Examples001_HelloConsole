5343c802a8f4abfe0adc8babf281c50dae6a933b
Andrey <andrejprasolov93239@gmail.com>
2022-06-27 05:32:42 +0300

    Написали приветствие, которое встречает определенного пользователя индивидуально

---
+++ Examples005_ifElse/Program.cs
@@ -0,0 +1,7 @@
+﻿String userName = Console.ReadLine();
+if(userName == "Андрей"){
+    Console.WriteLine("Ураа! Это же " + userName +".");
+} else{
+    Console.WriteLine("Привет, " + userName +"!");
+}
+
