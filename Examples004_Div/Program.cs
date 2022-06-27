58832434c48f6664fa60d03bc4e0c0f161b07761
Andrey <andrejprasolov93239@gmail.com>
2022-06-27 05:01:24 +0300

    Написал программу произведения двух случайных чисел

--- Examples004_Div/Program.cs
+++ Examples004_Div/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿int numberA = new Random().Next(1,500);// 1 2 3 4 5 6 7 8 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,500);// 1 2 3 4 5 6 7 8 9
+Console.WriteLine(numberB);
+Console.WriteLine(numberA * numberB);
+
