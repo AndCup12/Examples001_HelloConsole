f50a1da69b368b3dcda1d713caa1895374c7dda1
Andrey <andrejprasolov93239@gmail.com>
2022-06-29 19:21:36 +0300

    Разобрали две задачи 1. С использованием функции 2. С использованием массива и функции

---
+++ lectures/num_1/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int arg1, int arg2, int arg3){
+    int result = arg1;
+    if(arg1 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = new Random().Next(1, 100);
+int a2 = new Random().Next(1, 100);
+int a3 = new Random().Next(1, 100);
+int a4 = new Random().Next(1, 100);
+int a5 = new Random().Next(1, 100);
+int a6 = new Random().Next(1, 100);
+int a7 = new Random().Next(1, 100);
+int a8 = new Random().Next(1, 100);
+int a9 = new Random().Next(1, 100);
+
+// int max1 = Max(a1, a2, a3);
+// int max2 = Max(a4, a5, a6);
+// int max3 = Max(a7, a8, a9);
+// int max = Max(max1, max2, max3); сокращенный вариант записи на 23 строке.
+
+int max = Max(Max(a1, a2, a3), Max(a4, a5, a6), Max(a7, a8, a9));
+
+Console.WriteLine(max);
---
+++ lectures/num_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int arg1, int arg2, int arg3){
+    int result = arg1;
+    if(arg1 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+//              0 1 2 3 4 5 6 7 8 
+int [] array = {1,2,5,6,8,1,5884,3,4};
+
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6],array[7],array[8])
+);
+
+Console.WriteLine(max);
+
