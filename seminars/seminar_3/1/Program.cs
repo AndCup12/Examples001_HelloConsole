f48add5d084367d848906a45eab0e95f13c0dc23
Andrey <andrejprasolov93239@gmail.com>
2022-07-06 13:30:04 +0300

    написал программу выводящую диапазон возможных координат в той или иной четверти

---
+++ seminars/seminar_3/1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите X");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+string Quarter (int X, int Y) {
+    if (X > 0 && Y > 0) return "Первая четверть";
+    if (X < 0 && Y > 0) return "Вторая четверть";
+    if (X < 0 && Y < 0) return "Третья четверть";
+    if (X > 0 && Y < 0) return "Четвертая четверть";
+    return "Ввели неверные значения";
+}
+
+string result = Quarter(X, Y);
+Console.WriteLine(result);
+ 
---
+++ seminars/seminar_3/num_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (X и Y).
+Console.WriteLine("Введите номер четверти");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string Range(int q){
+    switch(q){
+        case 1: 
+            return "Диапазон возможных координат X > 0 и Y > 0";
+            break;
+        case 2:
+            return "Диапазон возможных координат X < 0 и Y > 0";
+            break;
+        case 3:
+            return "Диапазон возможных координат X < 0 и Y < 0";
+            break;
+        case 4:
+            return "Диапазон возможных координат X > 0 и Y < 0";
+            break;
+        default:
+        return "Введена некорректная четверь";
+        break;
+    }
+}
+string result = Range(quarter);
+Console.WriteLine(result);
+
