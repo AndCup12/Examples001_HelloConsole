6f5d27b796ce4c13fbd26afcea8dcb06dc88c022
Andrey <andrejprasolov93239@gmail.com>
2022-07-14 01:24:29 +0300

    Лекция 3. Функции и методы

---
+++ lectures/lecture_3/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿int [] array = {1,2,1,5,88,62,52};
+
+
+void printArray(int [] array){
+   int count = array.Length;
+   for (int i = 0; i < count; i++)
+   {
+        Console.Write($"{array[i]} ");
+   }
+   Console.WriteLine();
+}
+
+
+void sortArr (int [] array){
+    
+    for (int i = 0; i < array.Length - 1 ; i++){
+        int maxPosition = i;
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if(array[j] > array[maxPosition]) maxPosition = j;
+            }
+        int temporrary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporrary;
+    }
+}
+printArray(array);
+sortArr(array);
+printArray(array);
